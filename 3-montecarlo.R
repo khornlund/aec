56e8f41a6fa35db2a72c910da2741ae210420ba0
Karl Hornlund <karlhornlung@gmail.com>
2018-11-20 21:23:33 +1030

    First working version of montecarlo sim

--- 3-montecarlo.R
+++ 3-montecarlo.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(ggplot2)
 library(ggthemes)
 library(reshape2)
+setwd("~/R/AEC")
 
 jiggle <- function(tpp_df, year, p_jiggle, mu_size, sd_size) {
   
@@ -12,8 +13,8 @@ jiggle <- function(tpp_df, year, p_jiggle, mu_size, sd_size) {
   extract_s <- rnorm(n = nrow(df), mean = mu_size, sd = sd_size)
   
   # Get amounts of voters to move
-  df$LIB_M <- min(extract_i * extract_s * df$LIB_P, df$LIB_N)
-  df$ALP_M <- min(extract_i * extract_s * df$ALP_P, df$ALP_N)
+  df$LIB_M <- extract_i * pmin(extract_s * df$LIB_P, df$LIB_N)
+  df$ALP_M <- extract_i * pmin(extract_s * df$ALP_P, df$ALP_N)
   
   # Remove voters
   df$LIB_N_removed <- df$LIB_N - df$LIB_M
@@ -35,14 +36,14 @@ jiggle <- function(tpp_df, year, p_jiggle, mu_size, sd_size) {
     'ALP_N' = df$ALP_N_added) %>%
     mutate(
       Total = LIB_N + ALP_N,
-      LIB_P = LIB_N / Total,
-      ALP_P = ALP_N / Total,
+      LIB_P = LIB_N / Total, # risk: divide-by-zero => NA values
+      ALP_P = ALP_N / Total, # risk: divide-by-zero => NA values
       LIB_W = LIB_P > 0.5)
   
   df$LIB_W <- df$LIB_P > 0.5
   
-  seat_delta <- jig_df$LIB_W - df$LIB_W
-  return(seat_delta)
+  ALP_W_delta <- sum(jig_df$LIB_W, na.rm = TRUE) - sum(df$LIB_W, na.rm = TRUE)
+  return(ALP_W_delta)
 }
 
 data_fn <- 'data/aec/aec-hor-tpp-results.rds'
@@ -51,13 +52,15 @@ tpp_df <- readRDS(data_fn)
 set.seed(1)
 
 year     <- 2010 # year to simulate
-p_jiggle <- 0.8 # probability of a district having its pop shuffled
-#mu_size  <- 10000 # mean size of pop to shuffle
+p_jiggle <- 0.70 # probability of a district having its pop shuffled
+#mu_size  <- 20000 # mean size of pop to shuffle
 sd_size  <- 5000 # sd of size to shuffle
 
-for(mu_size in seq(10000, 20000, 50000)) {
-  results <- replicate(500, jiggle(tpp_df, year, p_jiggle, mu_size, sd_size))
-  print(str_c('Mu=', mu_size, ' , mean seat change=', mean(results)))
+for (mu_size in seq(20000, 35000, 5000)) {
+  results <- replicate(10000, jiggle(tpp_df, year, p_jiggle, mu_size, sd_size))
+  print(str_c('Mu=', mu_size, ' , mean seat change=', mean(results, na.rm = TRUE)))
 }
+
+
 
 
